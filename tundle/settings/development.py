226dba68466915fd097f3e58dffbc6125168e36b
Ludovic LEGENDART <ludovic.legendart@green-conseil.com>
2014-02-01 07:52:56 +0100

    Setting up the project structure

---
+++ tundle/settings/development.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""
+Development settings for tundle project.
+"""
+
+from tundle.settings.base import *
+
+INSTALLED_APPS += (
+    'debug_toolbar',
+)
---
+++ tundle/settings/production.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+"""
+Production settings for tundle project.
+"""
+
+from tundle.settings.base import *
